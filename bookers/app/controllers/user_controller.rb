545eccd9ca330ec631a991d3dbe3f15ac72409d6
616865taro <616865@W.jn2.iwasaki.ac.jp>
2019-12-01 11:42:26 +0900

    book/post

--- bookers/app/controllers/user_controller.rb
+++ bookers/app/controllers/user_controller.rb
@@ -1,4 +1,20 @@
 class UserController < ApplicationController
     def index
+        @book = Book.new
     end
+
+    def new
+    end
+
+    def create
+        book = Book.new(book_params)
+        book.save
+        redirect_to 
+    end
+
+    private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+
 end
--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
     get 'home/show' => 'home#show'
     resources :user
     devise_for :users
+    post 'books' => 'user/create'
 #   devise_scope :user do
 #     get 'profile_edit', to: 'users/registrations#profile_edit', as: 'profile_edit'
 #     patch 'profile_update', to: 'users/registrations#profile_update', as: 'profile_update'
 #   end
 #   get 'root/mypage' => 'root#mypage'
-#   post 'books' => 'root/create'
 #   get 'users/edit' => 'devise/resistration#edit'
 end
