e246b1074757b756b3d6fb7e1c4a380e332e7176
616865taro <616865@W.jn2.iwasaki.ac.jp>
2019-12-05 01:07:30 +0900

    sign_in/sign_up->profilepage

--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     get 'homes/show' => 'homes#show'
     devise_for :users
     resources :users
+    resources :profiles
 #   devise_scope :user do
 #     get 'profile_edit', to: 'users/registrations#profile_edit', as: 'profile_edit'
 #     patch 'profile_update', to: 'users/registrations#profile_update', as: 'profile_update'
