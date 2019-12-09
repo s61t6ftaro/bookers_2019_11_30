7366fb6ddb8994a36f1ef951ce9ace360f7f66a5
616865taro <616865@W.jn2.iwasaki.ac.jp>
2019-12-09 17:40:31 +0900

    記載忘れ

--- bookers/app/controllers/application_controller.rb
+++ bookers/app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < ActionController::Base
             user_path(resource)
         end
     end
+    def after_sign_out_path_for(resource)
+        flash[:success] = "success" 
+        root_path
+      end
+  end
     before_action :configure_permitted_parameters, if: :devise_controller?
     protected
     def configure_permitted_parameters
