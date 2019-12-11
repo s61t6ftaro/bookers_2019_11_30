11c50569a18c9d748c80fa078f93029f6da31bd7
616865taro <616865@W.jn2.iwasaki.ac.jp>
2019-12-12 01:35:51 +0900

    userIcon/userName/userIntroduction/add!

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -1,4 +1,18 @@
 class BooksController < ApplicationController
-    def index
+    def show
+        @book = Book.new
+        @book = Book.find(params[:id])
+    end
+    def create
+        @book = Book.new(book_params)
+        @book.user_id = current_user.id
+        @book.save
+        if @book.save
+            redirect_to book_path(@book)
+        end
+    end
+private
+    def book_params
+      params.require(:book).permit(:title, :body)
     end
 end
