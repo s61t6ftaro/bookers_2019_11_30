6aae6194ce9dd1e1dd0f5d3d5837231f00af746a
616865taro <616865@W.jn2.iwasaki.ac.jp>
2019-12-16 14:36:41 +0900

    user_post_books_list

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -1,8 +1,14 @@
 class BooksController < ApplicationController
+    def index
+        @book = Book.new
+        @books = Book.all
+    end
+
     def show
         @book = Book.new
         @book = Book.find(params[:id])
     end
+
     def create
         @book = Book.new(book_params)
         @book.user_id = current_user.id
--- bookers/app/controllers/users_controller.rb
+++ bookers/app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
+    def index
+        @book = Book.new       
+        @users = User.all
+    end
     def show
-        @book = Book.new
+        @book = Book.new       
     end
 
     def edit 
