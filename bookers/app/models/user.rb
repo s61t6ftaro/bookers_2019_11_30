7378fc3fef9620eee412206748e8c2e82a22251d
s61t6ftaro <bisk1280392@gn.iwasaki.ac.jp>
2020-01-20 19:42:09 +0900

    user_profile_edit/introduction error message cant output

--- bookers/app/models/user.rb
+++ bookers/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true, length:{minimum: 2, maximum: 20}
+  validates :introduction, length: {maximum: 50 },on: :update
   has_many :books,dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
