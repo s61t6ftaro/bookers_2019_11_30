6f392451c651ec2b0f7e7831a2e86dfad49c95a0
s61t6ftaro <bisk1280392@gn.iwasaki.ac.jp>
2020-01-23 16:33:47 +0900

    new bookers2

---
+++ bookers2/app/controllers/books_controller.rb
@@ -0,0 +1,46 @@
+class BooksController < ApplicationController
+    def index
+        @book = Book.new
+        @books = Book.all
+    end                                                  
+    def show
+        @book = Book.new
+        @book = Book.find(params[:id])
+    end
+
+    def edit
+        @book = Book.find(params[:id])
+    end
+
+    def create
+        @book = Book.new(book_params)
+        @book.user_id = current_user.id
+        @book.save
+        if @book.save
+            flash[:notice] = "You have creatad book successfully."
+            redirect_to book_path(@book.id)
+        else
+            @books = Book.all 
+            @user = current_user
+            render 'index'
+        end
+    end
+
+    def update
+        book = Book.find(params[:id])
+        book.update(book_params)
+        redirect_to book_path(book.id)
+        flash[:notice] = "You have updated book successfully."
+    end
+
+    def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+        redirect_to books_path
+    end
+
+private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+end
