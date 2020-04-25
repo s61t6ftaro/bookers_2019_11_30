10be632573285c1b5be2233e3f1a48371fc6d0f0
s61t6ftaro <bisk1280392@gn.iwasaki.ac.jp>
2020-04-25 18:49:34 +0900

    complete bookers2 (true)

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -15,13 +15,12 @@ class BooksController < ApplicationController
     end
 
     def create
-        @book = Book.new(book_params)
-        @book.user_id = current_user.id
-        if @book.save
+        @booknew = Book.new(book_params)
+        @booknew.user_id = current_user.id
+        if @booknew.save
             flash[:notice] = "You have creatad book successfully."
-            redirect_to book_path(@book.id)
+            redirect_to book_path(@booknew.id)
         else
-            @booknew = Book.new
             @books = Book.all 
             @user = current_user
             render 'index'
